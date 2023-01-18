0376967f71ee8f2c3e1fc4838f40b62aedf25da9
hugo pinheiro <hugo.pinheiro@toodoo.com.br>
2023-01-18 11:58:33 -0300

    add tabbar and BottomNavigationBar in screen One

--- lib/pages/one_page.dart
+++ lib/pages/one_page.dart
@@ -10,6 +10,91 @@ class OnePage extends StatefulWidget {
 class _OnePageState extends State<OnePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Center(
+          child: RichText(
+              text: const TextSpan(
+                  text: "Carta",
+                  style: TextStyle(color: Colors.red, fontSize: 20),
+                  children: <TextSpan>[
+                TextSpan(
+                  text: "Capital",
+                  style: TextStyle(color: Colors.black),
+                )
+              ])),
+        ),
+        leading: GestureDetector(
+          onTap: () {},
+          child: const Icon(Icons.menu, color: Colors.black),
+        ),
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                  size: 26.0,
+                ),
+              )),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+            ),
+            onPressed: () {
+              Navigator.of(context).restorablePushReplacementNamed('/one');
+            },
+            child: const Text("Assine"),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 4,
+          // onTap: (index) => setState(() => currentIndex = index), //corrigir isso
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.black,
+          selectedFontSize: 10,
+          iconSize: 25,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: "INÍCIO",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.mail,
+              ),
+              label: "REVISTAS",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.bookmark,
+              ),
+              label: "SALVOS",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notifications,
+              ),
+              label: "NEWS",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: "PERFIL",
+            ),
+          ]),
+    );
   }
 }
